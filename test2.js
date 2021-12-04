d711d79759281fc8447ca25d2d4e41061fb542e6
karens1494 <karen.suarez9414@gmail.com>
2021-12-04 13:55:06 -0600

    chore: Catch errors and return NotFound or ServerError components

--- test2.js
+++ test2.js
@@ -1,6 +1,19 @@
-const {  } = require('./test/utils/errors');
+const { ServerError, NotFound } = require("./test/utils/errors");
 
-module.exports = async function Test2 (server, queries) {
-    const response = await server.query(queries); // Will return an array with the results, empty array or error
-    return JSON.parse(response);
-}
+module.exports = async function Test2(server, queries) {
+  let response;
+  try {
+    response = await server.query(queries); // Will return an array with the results, empty array or error
+    response = JSON.parse(response);
+    if (!response.length) {
+      throw new Error("No data found.");
+    }
+  } catch (error) {
+    if (error.message === "No data found.") {
+      return new NotFound(error.message);
+    } else {
+      return new ServerError(error.message);
+    }
+  }
+  return response;
+};
